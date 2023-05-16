19be6cc504a8975370ebf96d0d7e2c77cea16449
Siphkazi21 <sphokiebango21@gmail.com>
2023-05-16 13:52:00 -0700

    C-Agrc, Agrv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+
+//main pront the name of the program
+//@argc: The number of arguments
+//@argv: array of argments
+//
+//always return to 0
+
+int main(argc, char *argv[])
+{
+        printf("%s\n", argv[%d]);
+        return(0);
+}
